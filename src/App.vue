d701ef197ad172a953c5e784bf2a331686c52351
Your Name <you@example.com>
2022-07-24 10:19:21 +0800

    “1”

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>333</h1>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
